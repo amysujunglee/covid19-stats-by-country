169d6beb8da790dd9cb15f1da937f0517a1bf682
amysujunglee <amy.sujunglee@gmail.com>
2020-09-20 20:31:58 -0700

    getting BC stats from API

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const result = document.querySelector(".result");
+const active = document.querySelector(".active");
+const confirmed = document.querySelector(".confirmed");
+const deaths = document.querySelector(".deaths");
+const date = document.querySelector(".date");
+
+function getCovidStats() {
+  fetch("https://api.covid19api.com/live/country/canada")
+    .then(res => res.json())
+    .then(data => {
+      let bcStats = data.filter(prov => prov.Province === "British Columbia");
+      console.log(bcStats);
+    })
+    .catch(err => console.log("error!"));
+}
+
+getCovidStats();
