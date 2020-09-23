7e5231fb3312ca25f14f7af0b09f376cd0978748
amysujunglee <amy.sujunglee@gmail.com>
2020-09-22 20:46:12 -0700

    adding global total stats

--- app.js
+++ app.js
@@ -1,75 +1,71 @@
 const globalLastUpdated = document.querySelector(".global-date");
+const totalConfirmed = document.querySelector(".total-confirmed");
+const totalDeaths = document.querySelector(".total-deaths");
+const totalRecovered = document.querySelector(".total-recovered");
 const newConfirmed = document.querySelector(".new-confirmed");
 const newDeaths = document.querySelector(".new-deaths");
 const newRecovered = document.querySelector(".new-recovered");
 
-const lastUpdated = document.querySelector(".date");
-const totalActive = document.querySelector(".total-active");
-const totalConfirmed = document.querySelector(".total-confirmed");
-const totalDeaths = document.querySelector(".total-deaths");
+const caLastUpdated = document.querySelector(".ca-date");
+const caActive = document.querySelector(".ca-active");
+const caConfirmed = document.querySelector(".ca-confirmed");
+const caDeaths = document.querySelector(".ca-deaths");
 
 const bcNewCases = document.querySelector(".bc-newcases");
 
+// total and new stats in global
 function getCovidStatsGlobal() {
   fetch("https://api.covid19api.com/summary")
     .then(res => res.json())
     .then(data => {
       console.log(data.Global);
       globalLastUpdated.innerHTML = data.Date.slice(0, 10);
+      totalConfirmed.innerHTML = data.Global.TotalConfirmed.toLocaleString(
+        "en"
+      );
+      totalDeaths.innerHTML = data.Global.TotalDeaths.toLocaleString("en");
+      totalRecovered.innerHTML = data.Global.TotalRecovered.toLocaleString(
+        "en"
+      );
       newConfirmed.innerHTML = data.Global.NewConfirmed.toLocaleString("en");
       newDeaths.innerHTML = data.Global.NewDeaths.toLocaleString("en");
       newRecovered.innerHTML = data.Global.NewRecovered.toLocaleString("en");
-
-      // data.map(function(item) {
-      //   if (item.Province === "British Columbia") {
-      //     active.innerHTML = item.Active;
-      //     confirmed.innerHTML = item.Confirmed;
-      //     deaths.innerHTML = item.Deaths;
-      //     date.innerHTML = item.Date;
-      //     console.log(item.Date);
-      //   }
-      // });
     })
 
-    .catch(err => console.log("error!"));
+    .catch(err => console.log("error in Global!"));
 }
 
+// total stats in Canada
 function getCovidStatsCanada() {
   fetch("https://api.covid19api.com/total/dayone/country/canada")
     .then(res => res.json())
     .then(data => {
-      const totalStats = data[data.length - 1];
-      totalActive.innerHTML = totalStats.Active.toLocaleString("en");
-      totalConfirmed.innerHTML = totalStats.Confirmed.toLocaleString("en");
-      totalDeaths.innerHTML = totalStats.Deaths.toLocaleString("en");
-      lastUpdated.innerHTML = totalStats.Date.slice(0, 10);
+      const caTotalStats = data[data.length - 1];
 
-      console.log(totalStats);
+      caLastUpdated.innerHTML = caTotalStats.Date.slice(0, 10);
+      caActive.innerHTML = caTotalStats.Active.toLocaleString("en");
+      caConfirmed.innerHTML = caTotalStats.Confirmed.toLocaleString("en");
+      caDeaths.innerHTML = caTotalStats.Deaths.toLocaleString("en");
 
-      // data.map(function(item) {
-      //   if (item.Province === "British Columbia") {
-      //     active.innerHTML = item.Active;
-      //     confirmed.innerHTML = item.Confirmed;
-      //     deaths.innerHTML = item.Deaths;
-      //     date.innerHTML = item.Date;
-      //     console.log(item.Date);
-      //   }
-      // });
+      console.log(caTotalStats);
     })
-    .catch(err => console.log("error!"));
+    .catch(err => console.log("error in Canada!"));
 }
 
+//BC new cases
 function getBcNewCases() {
   fetch(
     "https://api.covid19api.com/dayone/country/canada/status/confirmed/live"
   )
     .then(res => res.json())
     .then(data => {
-      //BC New Cases
       const bcStats = data.filter(item => item.Province === "British Columbia");
+
       const latestBcStats = bcStats[bcStats.length - 1];
       const secondBcStats = bcStats[bcStats.length - 2];
       const newCases = latestBcStats.Cases - secondBcStats.Cases;
+
+      console.log(bcStats);
 
       bcNewCases.innerHTML = newCases;
     });
